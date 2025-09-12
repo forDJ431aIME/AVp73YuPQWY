7438d8940c6bebaaf387faa3adc110073e6cc68a
forDJ431aIME <dill.mauls-0m@icloud.com>
2025-09-13 01:26:29 +0900

    Configure new 图片尺寸批量调整器 feature

---
+++ batch_image_resizer_0913_0026_uej.py
@@ -0,0 +1,11 @@
+# 代码生成时间: 2025-09-13 00:26:28
+import os
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.filechooser import FileChooserListView, FileChooserPopup
+from PIL import Image
+from PIL import ImageOps
+
+"
